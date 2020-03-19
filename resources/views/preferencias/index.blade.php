ebf41deeb9636f37105e7dcadb988ee2448cc3ec
Alberto Sierra <alberto.sierra@murciaeduca.es>
2020-03-19 08:07:04 +0100

    Mostrando los módulos en tablas

--- resources/views/preferencias/index.blade.php
+++ resources/views/preferencias/index.blade.php
@@ -17,25 +17,33 @@
     </div>
     <div class="title m-b-md">
         <h2>Puede elegir m&aacute;s módulos de la siguiente lista.</h2>
-        <ul>
+        <table>
+            <thead>
+                <th>Ciclo / M&oacute;dulo</th><th>Elegir</th>
+            </thead>
+            <tbody>
             @foreach ($ciclos as $ciclo)
-                <li><strong>{{ $ciclo->nombre }}</strong>
-                    <ul>
-                        @foreach ($ciclo->modulos as $modulo)
-                            @if(!in_array($modulo->codigo, $misModulosCodigo))
-                                <li><strong>{{ $modulo->nombre }}</strong> ({{ $modulo->especialidad->nombre }})
-                                    <form method="POST" action="/preferencias" class = "pull-right">
-                                        @csrf
-                                        <input type="hidden" name="modulo_codigo" value="{{ $modulo->codigo }}">
-                                        <input type="submit" value="+" class="btn btn-success">
-                                    </form>
-                                </li>
-                            @endif
-                        @endforeach
-                    </ul>
+                <tr>
+                    <td colspan="2"><strong>{{ $ciclo->nombre }}</strong></td>
+                </tr>
+                @foreach ($ciclo->modulos as $modulo)
 
-                </li>
+                    @if(!in_array($modulo->codigo, $misModulosCodigo))
+                <tr>
+                    <td><strong>{{ $modulo->nombre }}</strong> ({{ $modulo->especialidad->nombre }})</td>
+                    <td>
+                        <form method="POST" action="/preferencias" class = "pull-right">
+                            @csrf
+                            <input type="hidden" name="modulo_codigo" value="{{ $modulo->codigo }}">
+                            <input type="submit" value="+" class="btn btn-success">
+                        </form>
+                    </td>
+
+                </tr>
+                    @endif
+                @endforeach
             @endforeach
-        </ul>
+            </tbody>
+        </table>
     </div>
 @endsection
